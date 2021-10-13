0f5ffabb34236743b5910bf7282ff7c4394de914
pawan10 <pawandev1099@gmail.com>
2021-10-13 18:00:05 +0530

    3rd commit

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Authenticate
+ extends StatefulWidget {
+  const Authenticate
+  ({ Key? key }) : super(key: key);
+
+  @override
+  _Authenticate
+  State createState() => _Authenticate
+  State();
+}
+
+class _Authenticate
+State extends State<Authenticate
+> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text('authenticate'),
+    );
+  }
+}
